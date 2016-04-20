e8c52830c5aaed8ec0b3b412801faf77616d100a
Aaron <acarlo@paylocity.com>
2016-04-20 21:32:45 +0000

    lots of humor.js

--- humor.js
+++ humor.js
@@ -51,15 +51,15 @@ function addTrigger(triggers, messages) {
 }
 
 addTrigger([/(clowns|clown)/i], ["I hate clowns."]);
-addTrigger([/(beer|booze|drinks|drink|drunk)]/i, ["Alright, that's it, I'll see you Supremes at the fox n' hound.", "It's 5 o'clock somewhere."]);
+addTrigger([/(beer|booze|drinks|drink|drunk)]/i], ["Alright, that's it, I'll see you Supremes at the fox n' hound.", "It's 5 o'clock somewhere."]);
 addTrigger([/game of thrones/i], ["My favorite character is Tyrian.", "I could watch that Lannister kid get slapped by Tyrian all day.", "You know nothing, Jon Snow!", "Winter is coming, beeotch."]);
 addTrigger([/vegas/], ["Can't wait to get wasted in Vegas.", "Only thing that would make Vegas better? More booze.", "Hell yeah, Vegas!"]);
 addTrigger([/(donald|donald trump|trump)/], ["That wall just got 10 feet higher, McCue.", "McCue is going to make Supreme great again!", "When McCue gets his White Castle, he better bring me some."]);
 addTrigger([/(hillary|clinton|hillary clinton)/i], ["The lizard lady is watching you, Aaron.", "I found undeniable proof that Hillary is an alien. It's all over reddit.", "Arf! Arf! Arf!"]);
 addTrigger([/Monday/i], ["I love mondays."]);
 addTrigger([/bug/i], ["Can't we just write code without bugs?", "We wouldn't have a problem if you'd write your code without bugs.", "Did those silly developers even check the code for bugs?"]);
-addTrigger([/(stand up|standup)]/i, ["I'm not going to make it to the next standup ... hair ball...", "Let me know how standup goes... I'm taking a nap.", "I'd attend standup, but I have a massive hangover."]);
-addTrigger([/(sandbox|sand box|sandboxes)]/i, ["Uh, sorry guys, I thought that was the LITTER box...", "When you next use that sandbox, you're going to need a scoop.", "I'm using the sandbox right now. A little privacy please?"]);
+addTrigger([/(stand up|standup)/i], ["I'm not going to make it to the next standup ... hair ball...", "Let me know how standup goes... I'm taking a nap.", "I'd attend standup, but I have a massive hangover."]);
+addTrigger([/(sandbox|sand box|sandboxes)/i], ["Uh, sorry guys, I thought that was the LITTER box...", "When you next use that sandbox, you're going to need a scoop.", "I'm using the sandbox right now. A little privacy please?"]);
 
 export function humorMessages(message, rtm) {
     // <@U128LDPNC>
