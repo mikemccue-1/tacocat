4b710f7bf323c0b9bd6ab9d0d4a8009217e41235
Michael Snead <msnead@paylocity-do-not-spam.com>
2016-05-13 20:51:05 +0000

    More environments! More fun!

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -4,17 +4,44 @@ const request = require('request');
 const config = require('../config.json');
 
 const deployTriggers = [
-	/what(s|\'s| is) (on|deployed|deployed to) (copper|bronze)/i
+	/what(s|\'s| is) (on|deployed|deployed to) (copper|bronze|prod|demo|corp|tin|pewter)/i
+];
+const whatEnvironments = [
+	/what environments/i
+];
+const environments = [
+	"prododd", "prodeven", "corpodd", "corpeven", "demoodd", "demoeven", "tin", "pewter", "copper", "bronze"
 ];
 
 export function checkDeploymentMessages(message, rtm) {
-
+	if(whatEnvironments.some((exp) => exp.test(message.text))) {
+	  rtm.sendMessage("I know " + environments.join(', '),
+			  config.room,
+			  () => console.log('sent what rooms I know'));
+	}
 	if(deployTriggers.some((exp) => exp.test(message.text))) {
-		let env = message.text.toUpperCase().indexOf('COPPER') > -1 ? 'copper' : 'bronze';
-		rtm.sendMessage("I'm checkin' " + env + ", hold onto your tacos...", config.room, () => console.log('sent hang on checking'));
+		let env = environments.find((eName) => {
+		  if(message.text.toUpperCase().indexOf(eName.toUpperCase()) > -1) {
+		    return eName;
+		  } else {
+		    return false;
+		  }
+		});
+		if(env === undefined) {
+			rtm.sendMessage("What kind of environment is that?", config.room,
+					() => console.log('could not find environment'));
+			return;
+		}
+
+		rtm.sendMessage("I'm checkin', hold onto your tacos...", config.room, () => console.log('sent hang on checking'));
 		getLastPackageDeploymentDate(env, (dateStr) => {
-			let message = 'Packages on ' + env + ' were built on ' + dateStr;
-			rtm.sendMessage(message, config.room, () => console.log('Sent deployment date'));
+			if(dateStr === "unknown") {
+			  rtm.sendMessage("Uhh, I looked but I got a non-sensical response.", config.room,
+					  console.log('Sent unknown response from bundles url'));
+			} else {
+			  let message = 'Packages on ' + env + ' were built on ' + dateStr;
+			  rtm.sendMessage(message, config.room, () => console.log('Sent deployment date'));
+			}
 		});
 	}
 }
@@ -25,14 +52,18 @@ export function getLastPackageDeploymentDate(env, callBack) {
 	request({
 		url: config[env + 'BundlesUrl']
 	}, (err, resp, body) => {
-		var bodyObj = JSON.parse(body);
+		try {
+		  var bodyObj = JSON.parse(body);
 
-		var firstPackageName = Object.keys(bodyObj)[0];
-		var firstTimeStamp = bodyObj[firstPackageName].split('?')[1].split('=')[1];
-		var dateStr = new Date(+firstTimeStamp).toString();
-		console.log(firstTimeStamp);
-		console.log('calling back with',dateStr);
-		callBack(dateStr);
+		  var firstPackageName = Object.keys(bodyObj)[0];
+		  var firstTimeStamp = bodyObj[firstPackageName].split('?')[1].split('=')[1];
+		  var dateStr = new Date(+firstTimeStamp).toString();
+		  console.log(firstTimeStamp);
+		  console.log('calling back with',dateStr);
+		  callBack(dateStr);
+		} catch(e) {
+		  callBack("unknown");
+		}
 	});
 }
 
